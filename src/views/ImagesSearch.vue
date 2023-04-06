0088521d6514fae5da4bda69d081e312c354503f
yosuke-murayama <mura019e@gmail.com>
2023-04-06 21:38:22 +0900

    added searchForm in searchPage

--- src/views/ImagesSearch.vue
+++ src/views/ImagesSearch.vue
@@ -2,20 +2,43 @@
   <div>
     <p>SearchImages</p>
     <v-col cols="12" sm="9" offset-sm="1">
-      <v-row>
+      <v-row v-if="!isShow">
         <v-text-field 
           label="タグで検索"
-          v-model="tagName">
+          v-model="tagName"
+        >
           <v-icon slot="prepend">
             mdi-magnify
           </v-icon>
-        </v-text-field> 
+        </v-text-field>
+        <v-btn @click="isShow = !isShow">撮影日で検索</v-btn>
+      </v-row>
+
+      <v-row v-else>
+        <v-text-field 
+          label="撮影日で検索"
+          v-model="date"
+        >
+          <v-icon slot="prepend">
+            mdi-magnify
+          </v-icon>
+        </v-text-field>
+        <v-btn @click="isShow = !isShow">タグ名で検索</v-btn>
       </v-row>
     </v-col>
 
-    <v-row>
+    <v-row v-if="searchedPosts.length !== 0">
       <ImageLists
         v-for="post in searchedPosts" 
+        :key="post.id"
+        :post="post"
+        @toggle-favorite="toggleFavorite"
+      />
+    </v-row>
+
+    <v-row v-if="searchedDatePosts.length !==0">
+      <ImageLists
+        v-for="post in searchedDatePosts" 
         :key="post.id"
         :post="post"
         @toggle-favorite="toggleFavorite"
@@ -37,7 +60,10 @@ export default {
   },
   data() {
     return {
-      tagName: ''
+      tagName: '',
+      date: '',
+      isShow: false,
+      menu: false
     }
   },
   methods: {
@@ -48,6 +74,9 @@ export default {
   computed: {
     searchedPosts() {
       return this.posts.filter((post) => post.imgTags.includes(this.tagName));
+    },
+    searchedDatePosts() {
+      return this.posts.filter((post) => post.date === this.date);
     }
   }
 }
